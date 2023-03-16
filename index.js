c24aa5b4851f2c0019c212c5f03518f49297d738
JohnnyZ2005 <oduoyeayomide6@gmail.com>
2023-03-16 20:17:34 +0100

    Added an hover effect

--- index.js
+++ index.js
@@ -1,41 +1,41 @@
 let selectButton = document.querySelector('.select-grid');
-selectButton.addEventListener('click', grid);
-
-
-// declaring a global variable 
-let askForGrid;
-const container = document.querySelector('.container');
+selectButton.addEventListener('click', grid);    // calls the grid function 
 const gridCreated = document.querySelector('.grid-created')
 
-
 // function to create grid 
+let askForGrid;
 function grid(){
     askForGrid = prompt('Enter a gird size: "Note your Grid Size must not be 100"'); 
-    // function to create div
-    const number = askForGrid;
-    gridCreated.textContent = `${number}*${number}`;
-    function createGrid(){
-        container.style.gridTemplateColumns = `repeat(${number}, 1fr)`;
-        container.style.gridTemplateRows = `repeat(${number}, 1fr)`;
-        for (let i = 0; i <= number*number; i++){
-            const sqaureDivs = document.createElement('div');
-            sqaureDivs.classList.add('squares');
-            container.appendChild(sqaureDivs);
+
+    // condition for div greater than or equal to 100 
+    if (askForGrid >= 100){
+        gridCreated.textContent = 'Grid cannot be created';
+    }  else if (askForGrid == null || askForGrid == ''){
+        gridCreated.textContent = 'Enter a valid number';
+    }else {
+
+        // function to create div
+        const container = document.querySelector('.container');
+        const number = askForGrid;
+        gridCreated.textContent = `${number}*${number}`;
+        function createGrid(){
+            container.style.gridTemplateColumns = `repeat(${number}, 1fr)`;
+            container.style.gridTemplateRows = `repeat(${number}, 1fr)`;
+            for (let i = 0; i <= number*number; i++){
+                const sqaureDivs = document.createElement('div');
+                sqaureDivs.classList.add('squares');
+                container.appendChild(sqaureDivs); 
+            }
         }
+
+        createGrid()     // calling the function
+
+        // creating an hover function 
+        function hoverSquare(e){
+            e.target.style.backgroundColor = 'black';
+        }
+        let squareDivsList = document.querySelectorAll('.squares')
+        squareDivsList.forEach(square => {square.addEventListener('mouseover', hoverSquare)})
     }
-    createGrid();
 }     
-
-
-
-// creating a hover function 
-function hoverSquare(event){
-    event.target.style.backgroundColor  = "black";
-}
-
-let sqaureDivsList = document.querySelectorAll('.squares');
-sqaureDivsList.forEach(square => console.log(square))
-// sqaureDivsList.forEach(square => {square.addEventListener('mouseover', hoverSquare)});
-
-
 
